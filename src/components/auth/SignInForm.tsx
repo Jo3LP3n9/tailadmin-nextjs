e3bca36bdcbc6525dc3afe68d066120bcda17370
joelpeng <30919393+joelpeng@users.noreply.github.com>
2025-11-06 16:58:43 +0800

    update swagger/goadmin.json

--- src/components/auth/SignInForm.tsx
+++ src/components/auth/SignInForm.tsx
@@ -6,24 +6,19 @@ import Button from "@/components/ui/button/Button";
 import { ChevronLeftIcon, EyeCloseIcon, EyeIcon } from "@/icons";
 import Link from "next/link";
 import React, { useEffect, useState } from "react";
-import { CaptchaGet200Response, DtoCaptchaResp  } from "@/api/generated";
+import { DefaultApi } from "@/api/generated";
 
 
 export default function SignInForm() {
   const [showPassword, setShowPassword] = useState(false);
   const [isChecked, setIsChecked] = useState(false);
-  
+  const [captchaInfo, setCaptchaInfo] = useState('');
+  const [captchaId, setCaptchaId] = useState('');
+
+
+
   return (
     <div className="flex flex-col flex-1 lg:w-1/2 w-full">
-      <div className="w-full max-w-md sm:pt-10 mx-auto mb-5">
-        <Link
-          href="/"
-          className="inline-flex items-center text-sm text-gray-500 transition-colors hover:text-gray-700 dark:text-gray-400 dark:hover:text-gray-300"
-        >
-          <ChevronLeftIcon />
-          Back to dashboard
-        </Link>
-      </div>
       <div className="flex flex-col justify-center flex-1 w-full max-w-md mx-auto">
         <div>
           <div className="mb-5 sm:mb-8">
